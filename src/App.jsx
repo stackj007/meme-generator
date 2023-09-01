1e331b7cc04c2de2fa97ecd06f6bd5cec44a46b4
Ahmed Gamal <agamal0077@gmail.com>
2023-09-01 12:38:12 +0300

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import Header from './components/Header'
+
 function App() {
-  return (
-    <div className="w-screen h-screen bg-red-300"></div>
-  )
+  return <Header />
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <img src="/Face.png" />
+      <h3 className="header--logo_text">Meme Generator</h3>
+      <h4 className="header--title">
+        React Course-Project
+      </h4>
+    </header>
+  )
+}
+
+export default Header
