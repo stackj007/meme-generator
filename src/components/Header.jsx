d5cc9129c93f6c08c0497761ef3bc53073e3735e
Ahmed Gamal <agamal0077@gmail.com>
2023-09-01 14:29:30 +0300

    Header syling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import React from 'react'
+
 function Header() {
   return (
-    <header>
-      <img src="/Face.png" />
-      <h3 className="header--logo_text">Meme Generator</h3>
-      <h4 className="header--title">
+    <header className="h-14 bg-gradient-to-r from-purple-800 to-pink-500 flex items-center h-24 p-7">
+      <img src="/Face.png" className="h-7 mr-2" />
+      <h3 className="mr-auto text-white font-bold text-xl">
+        Meme Generator
+      </h3>
+      <h4 className="font-semibold">
         React Course-Project
       </h4>
     </header>
