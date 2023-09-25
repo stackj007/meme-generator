a98200585dc1f3e8d30c198b3fadc9db3e1627a7
Ahmed Gamal <agamal0077@gmail.com>
2023-09-25 13:48:20 +0300

    Getting memes from api

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import memeData from '../memeData'
 
 function Meme() {
   const [image, setImage] = React.useState({
@@ -8,14 +7,21 @@ function Meme() {
     bottomText: '',
   })
 
-  const [allMemes, setAllMemes] = React.useState(memeData)
+  const [allMemes, setAllMemes] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then((res) => res.json())
+      .then((data) => {
+        setAllMemes(data.data.memes)
+      })
+  }, [])
 
   function getMemeImg() {
-    const memesArray = allMemes.data.memes
     const randomIndex = Math.floor(
-      Math.random() * memesArray.length
+      Math.random() * allMemes.length
     )
-    const url = memesArray[randomIndex].url
+    const url = allMemes[randomIndex].url
 
     setImage((prevImage) => ({
       ...prevImage,
@@ -61,19 +67,6 @@ function Meme() {
         </button>
       </div>
 
-      {/* <div className="relative rounded-lg mt-6">
-        <img
-          className="max-w-md  border-md rounded-lg  p-6 mx-auto mt-6"
-          src={image.randomImg}
-        ></img>
-        <h2 className="text-2xl font-bold absolute top-5 left-1/2 text-center mt-4">
-          {image.topText}
-        </h2>
-        <h2 className="text-2xl font-bold absolute bottom-7 left-1/2 text-center mt-4">
-          {image.bottomText}
-        </h2>
-      </div> */}
-
       <div className="relative rounded-lg mt-6">
         <div className="flex justify-center items-center">
           <img
@@ -83,10 +76,10 @@ function Meme() {
           />
         </div>
 
-        <h2 className="absolute top-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-white text-2xl font-bold">
+        <h2 className="absolute top-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-white text-3xl font-bold">
           {image.topText}
         </h2>
-        <h2 className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl font-bold">
+        <h2 className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl font-bold">
           {image.bottomText}
         </h2>
       </div>
